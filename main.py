9520d076a28753e2c03a836732e33ed463d3526b
Memo Chou <memochou1993@gmail.com>
2024-11-09 14:41:29 +0800

    Add Supabase integration

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import os
 
-from fastapi import FastAPI, HTTPException
+import supabase
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from nanoid import generate
 
 from schema.link import LinkCreateRequest, LinkCreateResponse
 
+load_dotenv()
+
+
 app = FastAPI()
 
 
-links = dict()
+supabase_client = supabase.create_client(
+    os.getenv("SUPABASE_API_URL"),
+    os.getenv("SUPABASE_API_KEY"),
+)
 
 
 @app.get("/", tags=["Static"], response_class=HTMLResponse)
@@ -21,15 +29,28 @@ def read_root():
 @app.post("/api/links", tags=["Link"], response_model=LinkCreateResponse)
 def create_link(body: LinkCreateRequest):
     code = generate(size=8)
-    links[code] = body.link
 
-    return LinkCreateResponse(code=code)
+    result = (
+        supabase_client.from_("links")
+        .insert(
+            {
+                "code": code,
+                "link": str(body.link),
+            }
+        )
+        .execute()
+    )
+
+    return LinkCreateResponse(**result.data[0])
 
 
 @app.get("/{code}", tags=["Link"])
 def redirect(code: str):
-    link = links.get(code)
-    if link is None:
-        raise HTTPException(status_code=404, detail="Link not found")
+    result = supabase_client.from_("links").select("*").eq("code", code).execute()
 
-    return RedirectResponse(url=link)
+    if not result.data:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Thread not found")
+
+    url = result.data[0]["link"]
+
+    return RedirectResponse(url=url)
